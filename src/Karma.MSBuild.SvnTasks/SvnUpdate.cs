fc2011b038e5e46f8b740bb9c6d5977f45e6c699
Manuel Islas <trentcioran@gmail.com>
2011-07-31 18:58:29 -0500

    finizhed work on update command support

--- src/Karma.MSBuild.SvnTasks/SvnUpdate.cs
+++ src/Karma.MSBuild.SvnTasks/SvnUpdate.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.ObjectModel;
+using Microsoft.Build.Framework;
 using SharpSvn;
 
 namespace Karma.MSBuild.SvnTasks
@@ -6,13 +8,34 @@ namespace Karma.MSBuild.SvnTasks
     public class SvnUpdate : SvnTaskBase
     {
         /// <summary>
+        /// Indicates if only current directory or recursive across all directories
+        /// </summary>
+        public bool Recursive { get; set; }
+
+        /// <summary>
         /// Actual method to be executed for the implementing task
         /// </summary>
         /// <param name="client">The instance of the SVN client</param>
         /// <returns></returns>
         public override bool ExecuteCommand(SvnClient client)
         {
-            throw new NotImplementedException();
+            SvnUpdateArgs args = new SvnUpdateArgs();
+            args.Depth = Recursive ? SvnDepth.Infinity : SvnDepth.Children;
+
+            SvnUpdateResult updateResult;
+
+            bool success = client.Update(RepositoryPath, args, out updateResult);
+
+            if (updateResult.HasResultMap)
+            {
+                foreach (string key in updateResult.ResultMap.Keys)
+                {
+                    SvnUpdateResult result = updateResult.ResultMap[key];
+                    Log.LogMessage(MessageImportance.Normal, "[{0} - {1}]", key, result.Revision);
+                }
+            }
+
+            return success;
         }
     }
 }
--- tests/Karma.MSBuild.SvnTasks.Test/SvnUpdateFixture.cs
+++ tests/Karma.MSBuild.SvnTasks.Test/SvnUpdateFixture.cs
@@ -1,14 +1,87 @@
+using System;
+using System.IO;
+using Microsoft.Build.Framework;
 using NUnit.Framework;
+using Rhino.Mocks;
 
 namespace Karma.MSBuild.SvnTasks.Test
 {
     [TestFixture]
     public class SvnUpdateFixture
     {
-        [Test]
-        public void TestUpdate()
+        private void CheckoutProject(IBuildEngine engine, string path)
         {
+            SvnCheckout task = new SvnCheckout();
+            task.RepositoryPath = path;
+            task.RepositoryUrl = "http://karma-test-repository.googlecode.com/svn";
+            task.BuildEngine = engine;
 
+            task.Execute();
+        }
+
+        private void ModifyLocalRepository(string basePath)
+        {
+            File.Delete(Path.Combine(basePath, "trunk\\DocumentA.txt"));
+            File.Delete(Path.Combine(basePath, "trunk\\DocumentB.txt"));
+        }
+
+        private void AssertRepositoryNotUpdated(string basePath)
+        {
+            Assert.That(File.Exists(Path.Combine(basePath, "trunk\\DocumentA.txt")), Is.False);
+            Assert.That(File.Exists(Path.Combine(basePath, "trunk\\DocumentB.txt")), Is.False);
+        }
+
+        private void AssertRepositoryUpdated(string basePath)
+        {
+            Assert.That(File.Exists(Path.Combine(basePath, "trunk\\DocumentA.txt")), Is.True);
+            Assert.That(File.Exists(Path.Combine(basePath, "trunk\\DocumentB.txt")), Is.True);
+        }
+
+        [Test]
+        public void TestUpdateNormal()
+        {
+            MockRepository repository = new MockRepository();
+            IBuildEngine engine = repository.StrictMock<IBuildEngine>();
+
+            string path = string.Format("C:\\tmp\\{0}", DateTime.Now.Ticks);
+
+            CheckoutProject(engine, path);
+
+            ModifyLocalRepository(path);
+
+            SvnUpdate task = new SvnUpdate();
+            task.Username = "guest";
+            task.RepositoryPath = path;
+            task.BuildEngine = engine;
+
+            bool success = task.Execute();
+
+            Assert.That(success, Is.True);
+            AssertRepositoryNotUpdated(path);
+        }
+
+        [Test]
+        public void TestUpdateRecursive()
+        {
+            MockRepository repository = new MockRepository();
+            IBuildEngine engine = repository.StrictMock<IBuildEngine>();
+
+            string path = string.Format("C:\\tmp\\{0}", DateTime.Now.Ticks);
+
+            CheckoutProject(engine, path);
+
+            ModifyLocalRepository(path);
+
+            SvnUpdate task = new SvnUpdate();
+            task.Username = "guest";
+            task.RepositoryPath = path;
+            task.Recursive = true;
+            task.BuildEngine = engine;
+
+            bool success = task.Execute();
+
+            Assert.That(success, Is.True);
+            AssertRepositoryUpdated(path);
         }
     }
 }
